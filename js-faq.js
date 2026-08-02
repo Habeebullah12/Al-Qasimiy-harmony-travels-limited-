0907ae0287acd0cb8c3d4be7083b6b8180d7417d
Habeebullah12 <qasimhabeebulahi@gmail.com>
2026-08-02 15:20:24 +0100

    Add FAQ toggle functionality in js-faq.js

---
+++ js-faq.js
@@ -0,0 +1,21 @@
+const questions = document.querySelectorAll(".faq-question");
+
+questions.forEach(question => {
+
+    question.addEventListener("click", () => {
+
+        const answer = question.nextElementSibling;
+
+        if(answer.style.maxHeight){
+
+            answer.style.maxHeight = null;
+
+        }else{
+
+            answer.style.maxHeight = answer.scrollHeight + "px";
+
+        }
+
+    });
+
+});
